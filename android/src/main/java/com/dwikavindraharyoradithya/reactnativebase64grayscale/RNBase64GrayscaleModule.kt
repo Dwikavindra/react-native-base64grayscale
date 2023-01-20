7291d3844b00b6ba82285e85cfec808e35e0086b
Dwikavindra Haryo Radithya <dwikavindra.haryo.r@mail.ugm.ac.id>
2023-01-20 15:35:12 +0000

    New Update with Promise and Fixed Binary Image

--- android/src/main/java/com/dwikavindraharyoradithya/reactnativebase64grayscale/RNBase64GrayscaleModule.kt
+++ android/src/main/java/com/dwikavindraharyoradithya/reactnativebase64grayscale/RNBase64GrayscaleModule.kt
@@ -1,17 +1,13 @@
 package com.dwikavindraharyoradithya.reactnativebase64grayscale
 
+import android.annotation.TargetApi
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.BitmapFactory.Options
 import android.graphics.Color
+import android.os.Build
 import android.util.Base64
-import com.facebook.react.bridge.NativeModule
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Callback
-import com.facebook.react.bridge.Promise
+import androidx.annotation.RequiresApi
+import com.facebook.react.bridge.*
 import java.io.ByteArrayOutputStream
 import java.math.BigInteger
 
@@ -19,41 +15,76 @@ class RNBase64GrayscaleModule(reactContext: ReactApplicationContext) : ReactCont
 
     override fun getName() = "RNBase64GrayscaleModule"
     @ReactMethod
-    private fun base64Coloredtobase64Grayscale(base64: String, width: Int, height: Int, callback: Callback) {
-        val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true
-        val code = Base64.decode(base64, Base64.DEFAULT)
-        val test = BitmapFactory.decodeByteArray(code, 0, code.size)
-        options.inJustDecodeBounds = false
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888
-        val bmp_Copy = test.copy(Bitmap.Config.ARGB_8888, true)
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                // get one pixel color
-                val pixel = test.getPixel(x, y)
-                // retrieve color of all channels
-                val A = Color.alpha(pixel)
-                var R = Color.red(pixel)
-                var G = Color.green(pixel)
-                var B = Color.blue(pixel)
-                // take conversion up to one single value
-                B = (0.299 * R + 0.587 * G + 0.114 * B).toInt()
-                G = B
-                R = G
-                // set new pixel color to output bitmap
-                bmp_Copy.setPixel(x, y, Color.argb(A, R, G, B))
+    private fun base64Coloredtobase64Grayscale(base64: String, promise:Promise) {
+        try{
+            val options = BitmapFactory.Options()
+            options.inJustDecodeBounds = true
+            val code = Base64.decode(base64, Base64.DEFAULT)
+            val test = BitmapFactory.decodeByteArray(code, 0, code.size)
+            options.inJustDecodeBounds = false
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888
+            val bmp_Copy = test.copy(Bitmap.Config.ARGB_8888, true)
+            for (x in 0 until test.width) {
+                for (y in 0 until test.height) {
+                    // get one pixel color
+                    val pixel = test.getPixel(x, y)
+                    // retrieve color of all channels
+                    val A = Color.alpha(pixel)
+                    var R = Color.red(pixel)
+                    var G = Color.green(pixel)
+                    var B = Color.blue(pixel)
+                    // take conversion up to one single value
+                    B = (0.299 * R + 0.587 * G + 0.114 * B).toInt()
+                    G = B
+                    R = G
+                    // set new pixel color to output bitmap
+                    bmp_Copy.setPixel(x, y, Color.argb(A, R, G, B))
+                }
             }
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            bmp_Copy.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+            val byteArray = byteArrayOutputStream.toByteArray()
+            val encoded = Base64.encodeToString(byteArray, Base64.DEFAULT)
+            promise.resolve(encoded)
         }
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        bmp_Copy.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
-        val byteArray = byteArrayOutputStream.toByteArray()
-        val encoded = Base64.encodeToString(byteArray, Base64.DEFAULT)
-        callback.invoke(encoded)
+        catch(e:Throwable){
+            promise.reject(e);
+        }
     }
+    @TargetApi(Build.VERSION_CODES.O)
+    @RequiresApi(Build.VERSION_CODES.O)
     @ReactMethod
-    private fun base64tobinaryString(base64: String, callback: Callback) {
-        val decode = Base64.decode(base64, Base64.DEFAULT)
-        val binaryStr = BigInteger(1, decode).toString(2)
-        callback.invoke(binaryStr)
+    private fun base64GrayscaletobinaryImage(base64: String, promise:Promise) {
+        try{
+            val code = Base64.decode(base64, Base64.DEFAULT)
+            val test = BitmapFactory.decodeByteArray(code, 0, code.size)
+            var binaryImageString= StringBuilder() //uses string builder instead of concatenating because
+            // if we were to concat using += its slow and creates a new instance for every concat Stringbuilder doesn't
+            val n = Math.ceil(test.width.toDouble()/ 8).toInt();
+            val scaledBitmap= Bitmap.createScaledBitmap(test,n,test.height,false)
+            for (y in 0 until scaledBitmap.height) {
+                for (x in 0 until scaledBitmap.width) {
+                    // get one pixel color
+                    val pixel = test.getPixel(x, y);
+                    var A= Color.alpha(pixel).toFloat()
+                    var G= Color.green(pixel).toFloat()
+                    var R = Color.red(pixel).toFloat()
+                    var B = Color.blue(pixel).toFloat()
+                    var finalInt= Color.argb(A,R,G,B)
+                    if (finalInt > 0.0) {
+                        binaryImageString.append("1")
+
+                    } else {
+                        binaryImageString.append("0")
+
+                    }
+                }
+                binaryImageString.append("\n")
+            }
+            promise.resolve(binaryImageString.toString())}
+        catch(e:Throwable){
+            promise.reject(e)
+        }
     }
+
 }
